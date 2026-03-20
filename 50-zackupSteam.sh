e63fdd3a25315d467d0aff814bfab6db002fc8c7
Multi Purr Puss :3 <34041223+PurryPlatypus@users.noreply.github.com>
2026-03-20 18:14:42 +0100

    Add zackupSteam script for ZFS Steam backup

---
+++ 50-zackupSteam.sh
@@ -0,0 +1,54 @@
+zackupSteam () {
+  echo "zackupSteam - ZFS Steam Backup"
+
+  # detect destructive mode flag
+  DESTROY=false
+  if [[ "$1" == "--destroy" ]]; then
+    echo "destructive --destroy mode enabled"
+    DESTROY=true
+  fi
+
+  # initialise sudo, ask password
+  echo "sudo elevated privileges"
+  if ! sudo -v; then
+    echo "authentication failed: we're done here!"
+    exit 1
+  fi
+
+  # keep the sudo session alive, in the background
+  # this is required for long runtime / large snapshots
+  echo "starting sudo -v keepalive loop in the background"
+  (while sleep 60; do sudo -v; done) & SUDO_LOOP_PID=$!
+
+  # kill the sudo loop when the script ends
+  echo "clean up: setting EXIT trap for SUDO_LOOP_PID = $SUDO_LOOP_PID"
+  trap 'kill "$SUDO_LOOP_PID"' EXIT
+
+  #initialise variables
+  echo "initialising variables"
+  NOW=$($(which date) +"%Y%m%d%H%M%S") # quirk, due to conflicting alias
+  REMOTE_HOST="elitedesk.lan"
+  LOCAL_Steam="bigNVME/Steam"
+  LOCAL_Steam_CURRSNAP="$LOCAL_Steam@$NOW"
+  LOCAL_Steam_PREVSNAP=$(zfs list -H -t snapshot -o name -s creation $LOCAL_Steam | tail -n1)
+  REMOTE_Steam="tank/backup/Steam"
+  REMOTE_Steam_PREVSNAP=$(ssh $REMOTE_HOST "zfs list -H -t snapshot -o name -s creation $REMOTE_Steam" | tail -n1)
+
+  # create ZFS snapshot
+  echo "creating recursive snapshots LOCAL_Steam_CURRSNAP = $LOCAL_Steam_CURRSNAP"
+  sudo zfs snap -r $LOCAL_Steam_CURRSNAP
+
+  # ZFS send/receive incrementally
+  echo "sending recursive incremental snapshots $LOCAL_Steam_PREVSNAP → $LOCAL_Steam_CURRSNAP"
+  sudo zfs send -c -R -i $LOCAL_Steam_PREVSNAP $LOCAL_Steam_CURRSNAP | pv | ssh $REMOTE_HOST "zfs receive -u -v $REMOTE_Steam"
+
+  # destroy old snapshot
+  if $DESTROY; then
+    echo "destroying snapshot LOCAL_Steam_PREVSNAP = $LOCAL_Steam_PREVSNAP"
+    sudo zfs destroy -r $LOCAL_Steam_PREVSNAP
+    echo "destroying snapshot REMOTE_Steam_PREVSNAP = $REMOTE_Steam_PREVSNAP"
+    ssh $REMOTE_HOST "zfs destroy -r $REMOTE_Steam_PREVSNAP"
+  else
+    echo "skipping destruction of previous snapshots (use --destroy to enable)"
+  fi
+}

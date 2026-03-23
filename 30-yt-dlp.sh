9d4ba3cb1f13648ccc4e5c817593b5860b08ff45
Multi Purr Puss :3 <34041223+PurryPlatypus@users.noreply.github.com>
2026-03-23 19:13:15 +0100

    Add ythumbnail function to download YouTube thumbnails
    Added a function to fetch YouTube video thumbnails.

--- 30-yt-dlp.sh
+++ 30-yt-dlp.sh
@@ -6,6 +6,11 @@ alias update-deno='curl -fsSL https://deno.land/install.sh | sh'
 # qq-tab complete (adjacent keys on QWERT* keyboard) for frequently used commands
 alias qq-yt-dlp='yt-dlp --js-runtimes deno:/home/user/.deno/bin/deno'
 
+# fetch the current YouTube thumbnail of a video ID
+ytthumbnail () {
+  wget http://img.youtube.com/vi/$1/maxresdefault-$1.jpg
+}
+
 # old / deprecated variants, just keeping 'em around
 #alias update-yt-dlp='python3 -m pip install -U "yt-dlp[default]" --break-system-packages'
 #alias update-yt-dlp='python3 -m pip install -U "yt-dlp[default,curl-cffi]" --break-system-packages'
